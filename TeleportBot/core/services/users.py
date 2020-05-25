db888a9eab85c96f3a5d515bfd07928336179f9d
dmitriyl99 <dmitriyl1899@gmail.com>
2020-05-26 01:06:12 +0500

    Create user

--- TeleportBot/core/services/users.py
+++ TeleportBot/core/services/users.py
@@ -1,5 +1,16 @@
 """
 Users API service
 """
+from . import make_post_request
 
 ENTITY = 'users'
+
+
+def create_user(telegram_id: int, name: str, username: str, language: str):
+    payload = {
+        'telegram_id': telegram_id,
+        'name': name,
+        'username': username,
+        'language': language
+    }
+    return make_post_request(ENTITY, '', payload)
