2a69751d2a48e01b099d1b070ce20d709fcc6192
dmitriyl99 <dmitriyl1899@gmail.com>
2020-05-26 16:20:39 +0500

    Add id to fillable. Use Telegram ID instead autogenerated ID

--- TeleportApi/app/User.php
+++ TeleportApi/app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'language', 'username', 'telegram_id'
+        'name', 'email', 'password', 'language', 'username', 'telegram_id', 'id'
     ];
 
     /**
