456b0366b79447c547f52dcee1a3df252ec7b771
dmitriyl99 <dmitriyl1899@gmail.com>
2020-05-27 17:19:08 +0500

    Format data

---
+++ TeleportBot/core/resources/utils.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+
+
+def date_difference(older: datetime, newer: datetime):
+    """
+    Returns a difference between two dates
+    :param older: Older date
+    :param newer: Newer date
+    :return: Dictionary with differences
+    """
+    difference = newer - older
+    days = difference.days
+    hours = difference.seconds / 3600
+    minutes = difference.seconds % 3600 / 60
+    seconds = difference.seconds % 3600 % 60
+
+    return {
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+    }
+
+
+def date_difference_now(date_string: str):
+    newer = datetime.now()
+    older = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%S.%fZ')
+
+    return date_difference(older, newer)
