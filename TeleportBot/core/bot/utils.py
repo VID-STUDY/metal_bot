83ee0cd5e17e1ae8b3edc75f0091c63b5422bb61
dmitriyl99 <dmitriyl1899@gmail.com>
2020-05-27 17:17:10 +0500

    Add filter for account

--- TeleportBot/core/bot/utils.py
+++ TeleportBot/core/bot/utils.py
@@ -1,4 +1,5 @@
 from core.resources import strings, keyboards
+from telegram.ext import BaseFilter
 
 
 class Navigation:
@@ -10,3 +11,10 @@ class Navigation:
             menu_message = strings.get_string('start.welcome', language).format(username=user_name)
         menu_keyboard = keyboards.get_keyboard('menu', language)
         update.message.reply_text(menu_message, reply_markup=menu_keyboard)
+
+
+class Filters:
+    class AccountFilter(BaseFilter):
+        def filter(self, message):
+            return message.text and (strings.get_string('menu.cabinet', 'ru') in message.text or
+                                     strings.get_string('menu.cabinet', 'uz') in message.text)
