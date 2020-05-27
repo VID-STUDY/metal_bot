693db2cb48d801ae08ed2551c81fa8df980f9c49
dmitriyl99 <dmitriyl1899@gmail.com>
2020-05-27 17:17:53 +0500

    Get user info

--- TeleportBot/core/resources/strings.py
+++ TeleportBot/core/resources/strings.py
@@ -1,5 +1,6 @@
 import os
 import json
+from . import utils
 
 
 _basedir = os.path.abspath(os.path.dirname(__file__))
@@ -19,3 +20,16 @@ def get_string(key, language='ru') -> str:
         return _strings_uz.get(key, 'no_string')
     else:
         raise Exception('Invalid language')
+
+
+def get_user_info(user: dict) -> str:
+    user_info = get_string('account.id', user.get('language')) + user.get('id')
+    user_info += '\n'
+    days = utils.date_difference_now(user.get('created_at')).get('days')
+    user_info += get_string('account.days', user.get('language')) + days
+    user_info += '\n'
+    user_info += get_string('account.balance', user.get('language')) + user.get('balance')
+    user_info += '\n'
+    user_info += get_string('account.status', user.get('language')) + user.get('user_role')
+
+    return user_info
