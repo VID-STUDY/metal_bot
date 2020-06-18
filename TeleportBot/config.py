23403fa6a6bb473e6c9ea3b2773cda4a26540d92
dmitriyl99 <dmitriyl1899@gmail.com>
2020-06-18 15:54:04 +0500

    Add Telegram channel username and invite link

--- TeleportBot/config.py
+++ TeleportBot/config.py
@@ -8,3 +8,5 @@ class Config:
     API_TOKEN = os.getenv('API_TOKEN')
     APP_URL = os.getenv('APP_URL')
     ENVIRONMENT = os.getenv('ENVIRONMENT')
+    TELEGRAM_CHANNEL_USERNAME = os.getenv('TELEGRAM_CHANNEL_USERNAME')
+    TELEGRAM_CHANNEL_LINK = os.getenv('TELEGRAM_CHANNEL_LINK')
