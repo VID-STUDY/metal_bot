e9777fefb9e84529740f84ba8af7353ba6c2a82e
dmitriyl99 <dmitriyl1899@gmail.com>
2020-06-19 16:44:28 +0500

    Fix relations with user

--- TeleportApi/app/Resume.php
+++ TeleportApi/app/Resume.php
@@ -17,7 +17,7 @@ class Resume extends Model
      */
     public function user()
     {
-        return $this->hasOne(User::class);
+        return $this->belongsTo(User::class);
     }
 
     /**
--- TeleportApi/app/Vacation.php
+++ TeleportApi/app/Vacation.php
@@ -17,7 +17,7 @@ class Vacation extends Model
      */
     public function user()
     {
-        return $this->hasOne(User::class);
+        return $this->belongsTo(User::class);
     }
 
     /**
