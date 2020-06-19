6674a24ee2f4058d92fee74fadd63cf659f594f3
dmitriyl99 <dmitriyl1899@gmail.com>
2020-06-19 16:44:42 +0500

    Getting settings

--- TeleportApi/app/Http/Controllers/Admin/SettingsController.php
+++ TeleportApi/app/Http/Controllers/Admin/SettingsController.php
@@ -15,12 +15,7 @@ class SettingsController extends Controller
      */
     public function index()
     {
-        $settings = Settings::first();
-        if (!$settings) {
-            $settings = Settings::create(['employer_tariff_1' => 7000,
-                'employer_tariff_2' => 13000, 'employer_tariff_3' => 18000, 'contractor_tariff_1' => 1200,
-                'contractor_tariff_2' => 1100, 'contractor_tariff_3' => 1000]);
-        }
+        $settings = Settings::get();
         return view('admin.settings.index', compact('settings'));
     }
 
--- TeleportApi/app/Settings.php
+++ TeleportApi/app/Settings.php
@@ -11,4 +11,15 @@ class Settings extends Model
         'contractor_tariff_1', 'contractor_tariff_2', 'contractor_tariff_3',
         'faq', 'about', 'partners', 'news'
     ];
+
+    public static function get()
+    {
+        $settings = self::first();
+        if (!$settings) {
+            $settings = Settings::create(['employer_tariff_1' => 7000,
+                'employer_tariff_2' => 13000, 'employer_tariff_3' => 18000, 'contractor_tariff_1' => 1200,
+                'contractor_tariff_2' => 1100, 'contractor_tariff_3' => 1000]);
+        }
+        return $settings;
+    }
 }
