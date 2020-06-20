7ddb5c4853d382f56f03536b1e260e1913453e26
dmitriyl99 <dmitriyl1899@gmail.com>
2020-06-20 21:03:51 +0500

    Delete echo

--- TeleportBot/main.py
+++ TeleportBot/main.py
@@ -12,11 +12,6 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 
-def echo(update, context):
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
-
-
 def error(update, context):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
@@ -73,9 +68,6 @@ def main():
     dp.add_handler(support.support_conversation)
 
     dp.add_handler(CommandHandler("help", help))
-
-    # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
     # log all errors
     # dp.add_error_handler(error)
 
