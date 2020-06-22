e33a97ecc5e9cae2b7b475691573b6d3c7732e67
dmitriyl99 <dmitriyl1899@gmail.com>
2020-06-22 16:44:38 +0500

    Additional users info

--- TeleportApi/resources/views/admin/users/edit.blade.php
+++ TeleportApi/resources/views/admin/users/edit.blade.php
@@ -14,35 +14,74 @@
             <h3 class="block-title"><i class="fa fa-user-circle mr-5 text-muted"></i> {{ $user->name }}</h3>
         </div>
         <div class="block-content">
-            <form action="{{ route('admin.users.update', $user->id) }}" method="post" enctype="multipart/form-data">
-                @csrf
-                @method('put')
-                <div class="row">
-                    <div class="col-sm-12 col-md-6">
-                        <div class="form-group @error('name') is-invalid @enderror">
-                            <div class="form-material floating">
-                                <input type="text" name="name" id="name" value="{{ $user->name }}" class="form-control">
-                                <label for="name">Имя пользователя</label>
-                            </div>
-                            @error('name') <div class="invalid-feedback animated fadeInDown">{{ $message }}</div> @enderror
-                        </div>
-                    </div>
-                    <div class="col-sm-12 col-md-6">
-                        <div class="form-group @error('email') is-invalid @enderror">
-                            <div class="form-material floating">
-                                <input type="email" name="email" id="email" class="form-control" value="{{ $user->email }}">
-                                <label for="email">Email</label>
-                            </div>
-                            @error('email') <div class="invalid-feedback animated fadeInDown">{{ $message }}</div> @enderror
-                        </div>
-                    </div>
+            <div class="row">
+                <div class="col-sm-12 col-md-6">
+                    <h3 class="content-heading pt-0">Имя</h3>
+                    <p>{{ $user->name }}</p>
                 </div>
-                <div class="block-content mb-10">
-                    <div class="block-content text-right pb-10">
-                        <button class="btn btn-alt-success" type="submit">Сохранить</button>
-                    </div>
+                <div class="col-sm-12 col-md-6">
+                    <h3 class="content-heading pt-0">Telegram</h3>
+                    @if ($user->username)
+                        <a href="https://t.me/{{ $user->username }}" target="_blank" class="link-effect">{{ $user->username }}</a>
+                    @else
+                        <span class="badge-pill badge-warning">Юзернейм отсутствует</span>
+                    @endif
                 </div>
-            </form>
+            </div>
+            <div class="row">
+                <div class="col-sm-12 col-md-4">
+                    <h3 class="content-heading pt-0">Текущий статус</h3>
+                    <p>@if ($user->user_role == 'employer') <span class="badge-pill badge-info">Работодатель</span> @elseif($user->user_role == 'contractor') <span class="badge-pill badge-info">Соискатель</span> @endif</p>
+                </div>
+                <div class="col-sm-12 col-md-4">
+                    <h3 class="content-heading pt-0">Баланс сосикателя</h3>
+                    <p>{{ $user->balance_contractor }}</p>
+                    <p>Тариф: <span class="badge-pill badge-info">
+                            @if ($user->contractor_tariff == 'contractor_tariff_1')
+                                Размещение 1-ого резюме
+                            @elseif(($user->contractor_tariff == 'contractor_tariff_2'))
+                                Размещение 2-х резюме
+                            @elseif(($user->contractor_tariff == 'contractor_tariff_3'))
+                                Размещение 3-х резюме
+                            @else
+                                Тариф не выбран
+                            @endif
+                        </span></p>
+                </div>
+                <div class="col-sm-12 col-md-4">
+                    <h3 class="content-heading pt-0">Баланс работодателя</h3>
+                    <p>{{ $user->balance_employer }}</p>
+                    <p>Тариф: <span class="badge-pill badge-info">
+                            @if ($user->employer_tariff == 'employer_tariff_1')
+                                Размещение 1-ой вакансии
+                            @elseif(($user->employer_tariff == 'employer_tariff_2'))
+                                Размещение 2-х вакансий
+                            @elseif(($user->employer_tariff == 'employer_tariff_3'))
+                                Размещение 3-х вакансий
+                            @else
+                                Тариф не выбран
+                            @endif
+                        </span></p>
+                </div>
+            </div>
+            <div class="row mb-50">
+                <div class="col-sm-12 col-md-6">
+                    <h3 class="content-heading">Резюме</h3>
+                    <ul class="list-group list-group flush">
+                        @foreach($user->resumes as $resume)
+                            <a href="{{ route('admin.resumes.show', $resume->id) }}" class="link-effect list-group-item-action">{{ $resume->title }}</a>
+                        @endforeach
+                    </ul>
+                </div>
+                <div class="col-sm-12 col-md-6">
+                    <h3 class="content-heading">Вакансии</h3>
+                    <ul class="list-group list-group flush">
+                        @foreach($user->vacations as $vacation)
+                            <a href="{{ route('admin.vacations.show', $vacation->id) }}" class="link-effect list-group-item-action">{{ $vacation->title }}</a>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
         </div>
     </div>
     <form action="{{ route('admin.users.message', $user->id) }}" method="post">
@@ -62,13 +101,6 @@
             </div>
         </div>
     </form>
-
-
-{{--    <a href="{{ route('admin.users.statistics', $user->id) }}" class="block block-link-shadow">--}}
-{{--        <div class="block-content block-content-full my-50">--}}
-{{--            <div class="font-size-h3 font-w600 text-center">Посмотреть статистику действий</div>--}}
-{{--        </div>--}}
-{{--    </a>--}}
 @endsection
 @section('js')
     <script src="{{ asset('assets/js/plugins/select2/select2.full.min.js') }}"></script>
