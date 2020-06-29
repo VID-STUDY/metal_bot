01f17444408875c8a0f4a8503e9effc318f19bd3
dmitriyl99 <dmitriyl1899@gmail.com>
2020-06-29 13:59:52 +0500

    Fix string resource name

--- TeleportBot/core/bot/resumes/create.py
+++ TeleportBot/core/bot/resumes/create.py
@@ -90,7 +90,7 @@ def resume_description(update, context):
 def resume_contacts(update, context):
     language = context.user_data['user'].get('language')
     if strings.get_string('go_back', language) in update.message.text:
-        message = strings.get_string('vacation.create.description', language)
+        message = strings.get_string('resumes.create.description', language)
         update.message.reply_text(message, parse_mode=ParseMode.HTML)
         return DESCRIPTION
     context.user_data['resume']['contacts'] = update.message.text
