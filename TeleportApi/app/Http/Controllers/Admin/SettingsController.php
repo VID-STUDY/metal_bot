618edb088e81a74353d78a1614b3b7a6a053f403
dmitriyl99 <dmitriyl1899@gmail.com>
2020-07-03 18:15:55 +0500

    Format text

--- TeleportApi/app/Http/Controllers/Admin/SettingsController.php
+++ TeleportApi/app/Http/Controllers/Admin/SettingsController.php
@@ -31,6 +31,20 @@ class SettingsController extends Controller
         $data = $request->all();
         $data['about'] = str_replace('&nbsp;', ' ', $data['about']);
         $data['about'] = str_replace('&mdash;', '-', $data['about']);
+        $data['about_uz'] = str_replace('&nbsp;', ' ', $data['about_uz']);
+        $data['about_uz'] = str_replace('&mdash;', '-', $data['about_uz']);
+        $data['news'] = str_replace('&nbsp;', ' ', $data['news']);
+        $data['news'] = str_replace('&mdash;', '-', $data['news']);
+        $data['news_uz'] = str_replace('&nbsp;', ' ', $data['news_uz']);
+        $data['news_uz'] = str_replace('&mdash;', '-', $data['news_uz']);
+        $data['partners'] = str_replace('&nbsp;', ' ', $data['partners']);
+        $data['partners'] = str_replace('&mdash;', '-', $data['partners']);
+        $data['partners_uz'] = str_replace('&nbsp;', ' ', $data['partners_uz']);
+        $data['partners_uz'] = str_replace('&mdash;', '-', $data['partners_uz']);
+        $data['faq'] = str_replace('&nbsp;', ' ', $data['faq']);
+        $data['faq'] = str_replace('&mdash;', '-', $data['faq']);
+        $data['faq_uz'] = str_replace('&nbsp;', ' ', $data['faq_uz']);
+        $data['faq_uz'] = str_replace('&mdash;', '-', $data['faq_uz']);
         if (!$settings) {
             $settings = Settings::create(['employer_tariff_1' => 7000,
                 'employer_tariff_2' => 13000, 'employer_tariff_3' => 18000, 'contractor_tariff_1' => 1200,
