22e12c5c500932f237aa06ef057fdf1d3bba122b
dmitriyl99 <dmitriyl1899@gmail.com>
2020-07-07 14:18:17 +0500

    Do not user js data table

--- TeleportApi/resources/views/admin/categories/category.blade.php
+++ TeleportApi/resources/views/admin/categories/category.blade.php
@@ -67,13 +67,6 @@
     <script src="{{ asset('assets/js/plugins/datatables/dataTables.bootstrap4.min.js') }}"></script>
 
     <script>
-        jQuery('.js-dataTable-full').dataTable({
-            "order": [],
-            pageLength: 10,
-            lengthMenu: [[10, 20, 30, 50], [10, 20, 30, 50]],
-            autoWidth: true,
-            language: ru_datatable
-        });
         $('.position').change(function () {
             $.ajaxSetup({
                 headers: {
